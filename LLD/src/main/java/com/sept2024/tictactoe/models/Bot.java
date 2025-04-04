e7d7d9a459c8f633778e1aff5b54a86aba3ae7c4
NIRMAL KUMAR <nirmalkumar8273@gmail.com>
2025-04-04 10:20:05 +0530

    implemented Board, BOt, Cell and Player model classes.

--- LLD/src/main/java/com/sept2024/tictactoe/models/Bot.java
+++ LLD/src/main/java/com/sept2024/tictactoe/models/Bot.java
@@ -1,4 +1,19 @@
 package com.sept2024.tictactoe.models;
 
-public class Bot {
+public class Bot extends Player{
+
+    private BotDifficultyLevel botDifficultyLevel;
+
+    public BotDifficultyLevel getBotDifficultyLevel() {
+        return botDifficultyLevel;
+    }
+
+    public void setBotDifficultyLevel(BotDifficultyLevel botDifficultyLevel) {
+        this.botDifficultyLevel = botDifficultyLevel;
+    }
+
+    public Bot(String name, int id, Symbol symbol, PlayerType playerType, BotDifficultyLevel botDifficultyLevel) {
+        super(name, id, symbol, playerType);
+        this.botDifficultyLevel = botDifficultyLevel;
+    }
 }
--- LLD/src/main/java/com/sept2024/tictactoe/models/Player.java
+++ LLD/src/main/java/com/sept2024/tictactoe/models/Player.java
@@ -1,4 +1,48 @@
 package com.sept2024.tictactoe.models;
 
 public class Player {
+
+    private String name;
+    private int id;
+    private Symbol symbol;
+    private PlayerType playerType;
+
+    public Player(String name, int id, Symbol symbol, PlayerType playerType) {
+        this.name = name;
+        this.id = id;
+        this.symbol = symbol;
+        this.playerType = playerType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Symbol getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(Symbol symbol) {
+        this.symbol = symbol;
+    }
+
+    public PlayerType getPlayerType() {
+        return playerType;
+    }
+
+    public void setPlayerType(PlayerType playerType) {
+        this.playerType = playerType;
+    }
 }
